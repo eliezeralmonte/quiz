44cdfe5ae3196eec1239f02a84b32faf8734302b
Jose Almonte <eboy40@gmail.com>
2021-04-18 12:33:24 -0500

    updated js code to render elements on quiz container

--- assets/script.js
+++ assets/script.js
@@ -4,25 +4,36 @@ var highScoresButtonEl = document.querySelector("#highScoresButton");
 var timerEl = document.querySelector("#timer");
 var startButtonEl = document.querySelector("#startButton");
 var introEl = document.querySelector("#intro");
-
 //question variables //
 var quizQuestionContainerEl = document.querySelector("#quizQuestionContainer");
 var questionEl = document.querySelector("#question");
-var answerListEl = document.querySelector("#answerList");
-var submitButtonEl = document.querySelector("#submitButton");
-var answersCountEl = document.querySelector("#answersCount");
+var questionChoicesEl = document.querySelector(".questionsClass");
+// var answer0El = document.querySelector ("#answer0");
+// var answer1El = document.querySelector ("#answer1");
+// var answer2El = document.querySelector ("#answer2");
+// var answer3El = document.querySelector ("#answer3");
+// var btn0El = document.querySelector ("#btn0");
+// var btn1El = document.querySelector ("#btn1");
+// var btn2El = document.querySelector ("#btn2");
+// var btn3El = document.querySelector ("#btn3");
+var progressEl = document.querySelector("#progress");
+var answerResponseEl = document.querySelector("#answerResponse");
+
 //results//
-var resultsEl = document.querySelector("#results");
-var highScoresEl = document.querySelector("#highScores");
+var resultsContainerEl = document.querySelector("#resultsContainer");
+var highScoresContainerEl = document.querySelector("#highScoresContainer");
 
 // global variables || questions array or object//
-
 var userScore = 0;
+var currentQuestion = 0;
+var time = 0;
+
+//questions array
 var questions = [
     {   
         question: "What are the main primitive data types in Javascript?",
-        answers: ["string, number, bigint, boolean, undefined, symbol, and null", "Ghosts, Warhogs, Tanks and Banshees", "Blue Team, Noble Team, ODST, and Spartans", "Paragraphs, Text Blocks, Equations, and Functions"], 
-        correct: "string, number, bigint, boolean, undefined, symbol, and null"
+        answers: ["Strings, Numbers, BigInts, Boolean, Undefined, Symbol, and Null", "Ghosts, Warhogs, Tanks and Banshees", "Blue Team, Noble Team, ODST, and Spartans", "Paragraphs, Text Blocks, Equations, and Functions"], 
+        correct: "String, Number, Bigint, Boolean, Undefined, Symbol, and Null"
     },
     {
         question: "??", 
@@ -33,40 +44,66 @@ var questions = [
 
 // functions
 //------------------------------//
-// function to start quiz//
-function renderQuestionContainer() {
+function timerFunc(){
+    let setTimer = setInterval(function(){
+        time++;
+        timerEl.innerHTML = time;
+        if(time === 60) {
+            clearInterval(setTimer)
+        };
+    }, 1000);
+};
+
+timerFunc();
+
+// startQuiz function will hide the intro container and display the Quiz Question Container
+function startQuiz() {
+    // this will hide Intro container and display quiz question container
     introEl.setAttribute("style", "display: none");
     quizQuestionContainerEl.setAttribute("style", "display: block");
-    questionEl.innerHTML = questions[0].question;
+    // once done it will render elements within the quiz question container
+    renderQuestionContainer();
+};
 
-// creates list to display an li containing 2 elements, first element will be a p, second element will be a radio button 
-    let listEl = document.createElement("li");
-    listEl.setAttribute("style", "display: flex; flex-direction: column;");
-    answerListEl.append(listEl);
+// function to render question && answers //
+function renderQuestionContainer() {
+// make sure to empty our questions container before rendering 
 
-// this loop will create/set attributes and become appended to the li
+//display current question
+questionEl.innerHTML = questions[currentQuestion].question;
 
-    for(var i = 0; i <= questions[0].answers.length; i++) {
-        let questionText = document.createElement("p");
-        // let radioBtn = document.createElement("input");
-        // radioBtn.setAttribute("type","radio");
-        // radioBtn.setAttribute("style","margin-right: 5px; width: 10%;")
-        questionText.innerHTML = questions[0].answers[i];
-        listEl.append(questionText);
-        // listEl.appendChild(radioBtn);
-
-        
+//display answer choices
+    // make a for loop to go through the current questions answer choices
+    for(let i = 0; i < questions[currentQuestion].answers.length; i++) {
+        // make answerChoices variable and create button element
+        let answerChoices = document.createElement("button");
+        // take answerChoices variable set innerHTML to empty string
+        answerChoices.innerHTML = "";
+        // take answerChoices variable and add innerHTML the current loop's question text index number
+        answerChoices.innerHTML = questions[currentQuestion].answers[i];
+        // append the answerChoices to questionChoicesEl container
+        questionChoicesEl.append(answerChoices);
+        // take answerChoices variable and attach clickListeners to each button and then if clicked call a function to check if answer correct
+        answerChoices.onclick = compareResponse;
     }
 
 
-    // for (var i = 0; i <= questions[0].answers.length; i++) {
-    //     let questionLi = document.createElement ("li");
-    //     questionLi = questions[0].answers[i]; 
-    //     questionEl.appendChild(listEl);
-    // }
-}
 
-console.log(questions[0].answers)
+
+    
+};
+ 
+function compareResponse () {
+    // compares answerChoices to 'correct' property within question object    
+    if(this.innerHTML === questions[currentQuestion].correct) {
+        userScore ++;
+        answerResponseEl.innerHTML = `Correct!`
+    } else {
+        answerResponseEl.innerHTML = `False!`
+    }
+    console.log(userScore);
+};
 
 // event listener functions//
-startButtonEl.addEventListener("click", renderQuestionContainer);
+startButtonEl.addEventListener("click", startQuiz);
+// nextQuestionButtonEl.addEventListener("click", rendernextQuestion;
